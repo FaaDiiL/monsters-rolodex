8d2fce132c77b3caa544e5a6f1478d6a57e8f471
Fadil Al <fadil.alfarhan@gmail.com>
2020-03-02 16:12:12 +0100

    Updates

---
+++ precache-manifest.c90824353f02a981b60afb17031ba02f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d04dec6a45b5c9c12077739b30feff",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b99f429a7be6aa9deb8f",
+    "url": "/monsters-rolodex/static/css/main.3d3bf709.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b99f429a7be6aa9deb8f",
+    "url": "/monsters-rolodex/static/js/main.0ccd449b.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
